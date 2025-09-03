4ddb684d87066f4d191d0e689496a4b74b186259
Guillem <guillemsuetta1@gmail.com>
2025-09-02 22:45:48 -0300

    refactor(types): Define a strict type for JWT payload

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,5 +1,14 @@
-declare namespace Express {
-  export interface Request {
-    user?: any;
+import { UserRole } from '../../enums/roles.enum';
+
+declare global {
+  namespace Express {
+    export interface Request {
+      user?: UserPayload;
+    }
   }
+}
+
+export interface UserPayload {
+  id: string;
+  role: UserRole;
 }
